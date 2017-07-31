4c7a5eeab840d1679f4d1af2c246caab2f2fdd3b
Jonathan LA <jonathan.lucuix@gmail.com>
2017-07-31 03:47:38 -0700

    Add log processor

---
+++ src/traffic/log/Log.java
@@ -0,0 +1,29 @@
+package traffic.log;
+/**
+ * A container for a log parsed from in Apache common log format.
+ */
+public class Log
+{
+    public String host;
+    public String id;
+    public String authUser;
+    public String date;
+    public String request;
+    public String requestMethod;
+    public String requestUrl;
+    public String requestProtocol;
+    public String status;
+    public String bytes;
+    
+    public Log() {}
+    
+    /**
+     * Creates a log with placeholder fields
+     */
+    public Log(String placeholder)
+    {
+        host = id = authUser = date = request = 
+            requestMethod = requestUrl = requestProtocol = 
+            status = bytes = placeholder;
+    }
+}
---
+++ src/traffic/log/LogProcessor.java
@@ -0,0 +1,71 @@
+package traffic.log;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.io.input.TailerListenerAdapter;
+
+import traffic.monitor.MetricManager;
+
+/**
+ * Reads a log file in Apache format, parses its contents and processes its metrics
+ */
+public class LogProcessor extends TailerListenerAdapter
+{
+    /** Regex which extracts information about a log line */
+    private static final String LOG_PATTERN = "(\\S+) (\\S+) (\\S+) \\[(\\d{1,2}\\/[a-zA-Z]+\\/\\d+:\\d{2}:\\d{2}:\\d{2} [+\\-] ?\\d{4})\\] \"(.{1,})\" (\\d+) (\\d+)";
+    /** The number of fields in a log line */
+    private static final int LOG_FIELD_COUNT = 7;
+    
+    /** Monitors metrics from a log file */
+    private MetricManager metricManager;
+    
+    public LogProcessor(MetricManager metricManager)
+    {
+        this.metricManager = metricManager;
+    }
+    
+    /**
+     * Called whenever a line is added to the log file
+     * @param line The line added to the log file
+     */
+    public void handle(String line)
+    {
+        Log log = parseLine(line);
+        metricManager.analyze(log);
+    }
+    
+    /**
+     * Returns a log object parsed from the given log line
+     * @param logLine The log line to parse
+     * @return A log object containing the information from the log line
+     */
+    private Log parseLine(String logLine)
+    {    
+        Pattern pattern = Pattern.compile(LOG_PATTERN);
+        Matcher matcher = pattern.matcher(logLine);
+        if (matcher.find() && matcher.groupCount() == LOG_FIELD_COUNT)
+        {
+            Log log = new Log();
+            
+            log.host = matcher.group(1);
+            log.id = matcher.group(2);
+            log.authUser = matcher.group(3);
+            log.date = matcher.group(4);
+            
+            // Extract request
+            log.request = matcher.group(5);
+            String[] requestFields = log.request.split(" ");
+            log.requestMethod = requestFields[0];
+            log.requestUrl = requestFields[1];
+            log.requestProtocol = requestFields[2];
+            
+            log.status = matcher.group(6);
+            log.bytes = matcher.group(7);
+            
+            return log;
+        }
+
+        // Error
+        return null;
+    }
+}
